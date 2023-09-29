662ff61c81a0fa29682711878bcd663466fc7494
bloominolive <bloomin.olive88@gmail.com>
2023-09-28 22:39:05 -0400

    added else in if statements to error handling

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -15,14 +15,17 @@ const getSingle = async (req, res) => {
     //#swagger.tags-['Dogs']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid dog id to find a dog.');
-      }
-    const dogId = new ObjectId(req.params.id);
-    try{
-    const result = await mongodb.getDatabase().db().collection('dogs').find({_id: dogId}).toArray();
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(result[0]);
-    }catch (err) {
-        res.status(400).json({ message: err });
+    }
+    else
+    {
+        const dogId = new ObjectId(req.params.id);
+        try{
+        const result = await mongodb.getDatabase().db().collection('dogs').find({_id: dogId}).toArray();
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(result[0]);
+        }catch (err) {
+            res.status(400).json({ message: err });
+        }
     }
 };
 
@@ -51,38 +54,45 @@ const updateDog = async (req, res) => {
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid dog id to update a dog.');
       }
-    const dogId = new ObjectId(req.params.id);
-    const dog = {
-        breed: req.body.breed,
-        group: req.body.group,
-        height: req.body.height,
-        weight: req.body.weight,
-        lifeExpectancy: req.body.lifeExpectancy,
-        shedLevel: req.body.shedLevel,
-        coatLength: req.body.coatLength,
-        goodBoyLevel: req.body.goodBoyLevel
-    };
-    const response = await mongodb.getDatabase().db().collection('dogs').replaceOne({_id: dogId}, dog);
-    if (response.modifiedCount > 0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred updating the dog.');
+      else
+      {
+        const dogId = new ObjectId(req.params.id);
+        const dog = {
+            breed: req.body.breed,
+            group: req.body.group,
+            height: req.body.height,
+            weight: req.body.weight,
+            lifeExpectancy: req.body.lifeExpectancy,
+            shedLevel: req.body.shedLevel,
+            coatLength: req.body.coatLength,
+            goodBoyLevel: req.body.goodBoyLevel
+        };
+        const response = await mongodb.getDatabase().db().collection('dogs').replaceOne({_id: dogId}, dog);
+        if (response.modifiedCount > 0){
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred updating the dog.');
+        }
     }
 };
+    
 
 const deleteDog = async (req, res) => {
     //#swagger.tags-['dogs']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid dog id to delete a dog.');
-      }
-    const dogId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('dogs').deleteOne({_id: dogId});
-    if (response.deletedCount > 0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred deleting the dog.');
     }
-}
+    else
+    {
+        const dogId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('dogs').deleteOne({_id: dogId});
+        if (response.deletedCount > 0){
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred deleting the dog.');
+        }
+    }
+};
 
 
 module.exports = {
--- controllers/springs.js
+++ controllers/springs.js
@@ -15,14 +15,17 @@ const getSingle = async (req, res) => {
     //#swagger.tags-['Springs']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid spring id to find a spring.');
-      }
-    const springId = new ObjectId(req.params.id);
-    try{
-    const result = await mongodb.getDatabase().db().collection('fl_springs').find({_id: springId}).toArray();
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(result[0]);
-    }catch (err) {
-        res.status(400).json({ message: err });
+    }
+    else
+    {
+        const springId = new ObjectId(req.params.id);
+        try{
+        const result = await mongodb.getDatabase().db().collection('fl_springs').find({_id: springId}).toArray();
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(result[0]);
+        }catch (err) {
+            res.status(400).json({ message: err });
+        }
     }
 };
 
@@ -43,36 +46,43 @@ const createSpring = async (req, res) => {
 
 const updateSpring = async (req, res) => {
     //#swagger.tags-['Springs']
-    if (!ObjectId.isValid(req.params.id)) {
+    if (!ObjectId.isValid(req.params.id)) 
+    {
         res.status(400).json('Must use a valid spring id to update a spring.');
-      }
-    const springId = new ObjectId(req.params.id);
-    const spring = {
-        name: req.body.name,
-        fees: req.body.fees,
-        county: req.body.county
-    };
-    const response = await mongodb.getDatabase().db().collection('fl_springs').replaceOne({_id: springId}, spring);
-    if (response.modifiedCount > 0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred updating the spring.');
     }
+    else
+    {
+        const springId = new ObjectId(req.params.id);
+        const spring = {
+            name: req.body.name,
+            fees: req.body.fees,
+            county: req.body.county
+        };
+        const response = await mongodb.getDatabase().db().collection('fl_springs').replaceOne({_id: springId}, spring);
+        if (response.modifiedCount > 0){
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred updating the spring.');
+        }
+    }
+    
 };
 
 const deleteSpring = async (req, res) => {
     //#swagger.tags-['Springs']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid spring id to delete a spring.');
-      }
-    const springId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('fl_springs').deleteOne({_id: springId});
-    if (response.deletedCount > 0){
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred deleting the spring.');
     }
-}
+    else{
+        const springId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('fl_springs').deleteOne({_id: springId});
+        if (response.deletedCount > 0){
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred deleting the spring.');
+        }
+    }
+};
 
 
 module.exports = {
