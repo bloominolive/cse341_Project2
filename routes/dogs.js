5d4874b2e289089f28b02c88972f802b7f2aa9d9
bloominolive <bloomin.olive88@gmail.com>
2023-10-02 16:23:26 -0400

    authentication complete

--- routes/dogs.js
+++ routes/dogs.js
@@ -4,14 +4,17 @@ const router = express.Router();
 const dogController = require('../controllers/dogs');
 const validation = require('../middleware/validate');
 
+const { isAuthenticated } = require("../middleware/authenticate");
+
+
 router.get('/', dogController.getAll);
 
 router.get('/:id', dogController.getSingle);
 
-router.post('/', validation.saveDog, dogController.createDog);
+router.post('/', validation.saveDog, isAuthenticated, dogController.createDog);
 
-router.put('/:id', validation.saveDog, dogController.updateDog);
+router.put('/:id', validation.saveDog, isAuthenticated, dogController.updateDog);
 
-router.delete('/:id', dogController.deleteDog);
+router.delete('/:id', isAuthenticated, dogController.deleteDog);
 
 module.exports = router;
--- routes/springs.js
+++ routes/springs.js
@@ -4,14 +4,16 @@ const router = express.Router();
 const springsController = require('../controllers/springs');
 const validation = require('../middleware/validate');
 
+const { isAuthenticated } = require("../middleware/authenticate");
+
 router.get('/', springsController.getAll);
 
 router.get('/:id', springsController.getSingle);
 
-router.post('/', validation.saveSpring, springsController.createSpring);
+router.post('/', validation.saveSpring, isAuthenticated, springsController.createSpring);
 
-router.put('/:id', validation.saveSpring, springsController.updateSpring);
+router.put('/:id', validation.saveSpring, isAuthenticated, springsController.updateSpring);
 
-router.delete('/:id', springsController.deleteSpring);
+router.delete('/:id', isAuthenticated, springsController.deleteSpring);
 
 module.exports = router;
