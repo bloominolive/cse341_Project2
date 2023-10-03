1b729c3d04fdd8b8e4ce90db0a7be5d78db35a18
bloominolive <bloomin.olive88@gmail.com>
2023-10-02 21:16:06 -0400

    logout page fix

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.use('/springs', require('./springs'));
 
 router.get('/login', passport.authenticate('github'), (req, res) => {});
 
-router.get('logout', function(req, res, next) {
+router.get('/logout', function(req, res, next) {
     req.logout(function(err) {
         if (err) { return next(err); }
         res.redirect('/');
